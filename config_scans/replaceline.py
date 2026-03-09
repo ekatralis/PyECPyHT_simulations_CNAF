95df3dc0c864c431f6bcea54fa855f70f81d1b7b
Evangelos Katralis <vkatralis@gmail.com>
2026-03-09 12:01:06 +0100

    Update replaceline so that it can be used from the command line

--- config_scans/replaceline.py
+++ config_scans/replaceline.py
@@ -23,3 +23,12 @@ def replaceline_and_save(fname, findln, newline):
     
     with open(fname, 'w') as fid:
         fid.writelines(lines)
+
+if __name__ == "__main__":
+   import argparse
+   parser = argparse.ArgumentParser(prog = "replaceline and save")
+   parser.add_argument("filename", help="Select path to filename", type = str)
+   parser.add_argument("findline", help="Line to replace", type = str)
+   parser.add_argument("newline", help="Line to append to file", type = str)
+   args = parser.parse_args()
+   replaceline_and_save(args.filename,args.findline,args.newline)
